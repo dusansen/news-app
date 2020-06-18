fa6544b7efcfc0954496f1bba07ee49346e66f3d
brnic <dusansen@dbrnic-nb.bravosystems.net>
2020-06-18 14:14:26 +0200

    calculate slides per page

--- news-app/src/components/articles/ArticlesCarousel.jsx
+++ news-app/src/components/articles/ArticlesCarousel.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Carousel from '@brainhubeu/react-carousel';
 import '@brainhubeu/react-carousel/lib/style.css';
 import Thumbnail from './Thumbnail';
 import LeftArrow from '../arrows/LeftArrow';
 import RightArrow from '../arrows/RightArrow';
+import useWindowSize from '../../hooks/useWindowSize';
 
 const ArticlesCarousel = ({ articles }) => {
+  const [slidesPerPage, setSlidesPerPage] = useState(1);
+  const size = useWindowSize();
+
+  useEffect(() => {
+    calculateSlidesPerPage(size.width);
+  }, [size]);
+  
+  const calculateSlidesPerPage = windowWidth => {
+    const numberOfSliderPerPage = windowWidth < 768 ?
+      1.2 :
+      windowWidth < 1200 ?
+        2.2 :
+        3.2;
+    setSlidesPerPage(numberOfSliderPerPage);
+  };
+
   return (
     <Carousel
-      slidesPerPage={3.2}
+      slidesPerPage={slidesPerPage}
       offset={16}
       arrows={true}
       arrowLeft={<LeftArrow />}
